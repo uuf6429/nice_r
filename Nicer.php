cd0795d054f977927031286b52f45c9d953f58f9
Christian Sciberras <uuf6429@gmail.com>
2014-06-23 01:56:39 +0200

    Added support for method/callback inspection.

--- Nicer.php
+++ Nicer.php
@@ -31,17 +31,30 @@
 		public $js_func = 'nice_r_toggle';
 		
 		/**
+		 * Whether to inspect and output methods for objects or not.
+		 * @var boolean
+		 */
+		public $inspect_methods = false;
+		
+		/**
 		 * Since PHP does not support private constants, we'll have to settle for private static fields.
 		 * @var string
 		 */
 		protected static $BEEN_THERE = '__NICE_R_INFINITE_RECURSION_PROTECT__';
 		
+		protected $_has_reflection = null;
+		
 		/**
 		 * Constructs new renderer instance.
 		 * @param mixed $value The value to inspect and render.
+		 * @param boolean $inspectMethods Whether to inspect and output methods for objects or not.
 		 */
-		public function __construct($value){
+		public function __construct($value, $inspectMethods = false){
 			$this->value = $value;
+			$this->inspect_methods = $inspectMethods;
+			
+			if(is_null($this->_has_reflection))
+				$this->_has_reflection = class_exists('ReflectionClass');
 		}
 		
 		/**
@@ -68,6 +81,51 @@
 			return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 		}
 		
+		protected function _inspect_array(&$html, &$var){
+			$has_subitems = false;
+			
+			foreach($var as $k => $v){
+				if($k !== self::$BEEN_THERE){
+					$html .= $this->_generate_keyvalue($k, $v);
+					$has_subitems = true;
+				}
+			}
+			
+			if(!$has_subitems){
+				$html .= '<span class="'.$this->css_class.'_ni">Empty Array</span>';
+			}
+		}
+		
+		protected function _inspect_object(&$html, &$var){
+			// render properties
+			$has_subitems = false;
+			
+			foreach((array)$var as $k=>$v){
+				if($k !== self::$BEEN_THERE){
+					$html .= $this->_generate_keyvalue($k, $v);
+					$has_subitems = true;
+				}
+			}
+			
+			if(!$has_subitems){
+				$html .= '<span class="'.$this->css_class.'_ni">No Properties</span>';
+			}
+
+			// render methods (if enabled)
+			if($this->inspect_methods){
+				$has_subitems = false;
+				
+				foreach((array)get_class_methods($var) as $method){
+					$html .= $this->_generate_callable($var, $method);
+					$has_subitems = true;
+				}
+				
+				if(!$has_subitems){
+					$html .= '<span class="'.$this->css_class.'_ni">No Methods</span>';
+				}
+			}
+		}
+		
 		/**
 		 * Render a single particular value.
 		 * @param mixed $var The value to render
@@ -88,16 +146,7 @@
 						$html .= '<span class="'.$this->css_class.'_ir">Infinite Recursion Detected!</span>';
 					}else{
 						$var[$BEENTHERE] = true;
-						$has_subitems = false;
-						foreach($var as $k=>$v){
-							if($k!==$BEENTHERE){
-								$html .= $this->_generate_keyvalue($k, $v);
-								$has_subitems = true;
-							}
-						}
-						if(!$has_subitems){
-							$html .= '<span class="'.$this->css_class.'_ni">Empty Array</span>';
-						}
+						$this->_inspect_array($html, $var);
 						unset($var[$BEENTHERE]);
 					}
 					break;
@@ -108,16 +157,7 @@
 						$html .= '<span class="'.$this->css_class.'_ir">Infinite Recursion Detected!</span>';
 					}else{
 						$var->$BEENTHERE = true;
-						$has_subitems = false;
-						foreach((array)$var as $k=>$v){
-							if($k!==$BEENTHERE){
-								$html .= $this->_generate_keyvalue($k, $v);
-								$has_subitems = true;
-							}
-						}
-						if(!$has_subitems){
-							$html .= '<span class="'.$this->css_class.'_ni">No Properties</span>';
-						}
+						$this->_inspect_object($html, $var);
 						unset($var->$BEENTHERE);
 					}
 					break;
@@ -132,13 +172,23 @@
 		}
 		
 		/**
+		 * Generates a new unique ID for tagging elements.
+		 * @staticvar int $id
+		 * @return integer An ID unique per request.
+		 */
+		protected function _generate_dropid(){
+			static $id = 0;
+			return ++$id;
+		}
+		
+		/**
 		 * Render a key-value pair.
 		 * @staticvar int $id Specifies element id.
 		 * @param string $key Key name.
 		 * @param mixed $val Key value.
 		 */
 		protected function _generate_keyvalue($key, $val){
-			static $id = 0; $id++;  // unique (per rquest) id
+			$id = $this->_generate_dropid();
 			$p = '';                // preview
 			$d = '';                // description
 			$t = gettype($val);     // get data type 
@@ -170,7 +220,7 @@
 			
 			$cls = $this->css_class;
 			$xcls = !$is_hash ? $cls.'_ad' : '';
-			$html  = '<a href="javascript:;" onclick="'.$this->js_func.'(\''.$this->html_id.'\',\''.$id.'\');">';
+			$html  = '<a '.($is_hash?'href="javascript:;"':'').' onclick="'.$this->js_func.'(\''.$this->html_id.'\',\''.$id.'\');">';
 			$html .= '	<span class="'.$cls.'_a '.$xcls.'" id="'.$this->html_id.'_a'.$id.'">&#9658;</span>';
 			$html .= '	<span class="'.$cls.'_k">'.$this->_esc_html($key).'</span>';
 			$html .= '	<span class="'.$cls.'_d">(<span>'.ucwords($t).'</span>'.$d.')</span>';
@@ -179,6 +229,59 @@
 			
 			if($is_hash){
 				$html .= $this->_generate_value($val, $cls.'_v', $this->html_id.'_v'.$id);
+			}
+			
+			return $html;
+		}
+		
+		protected function _generate_callable($context, $callback){
+			$id = $this->_generate_dropid();
+			$ref = null;
+			$name = 'Anonymous';
+			$cls = $this->css_class;
+			
+			if($this->_has_reflection){
+				if(is_null($context)){
+					$ref = new ReflectionFunction($callback);
+				}else{
+					$ref = new ReflectionMethod($context, $callback);
+				}
+				$name = $ref->getName();
+			}elseif(is_string($callback)){
+				$name = $callback;
+			}
+				
+			if(!is_null($ref)){
+				$doc = $ref->getDocComment();
+				$prms = array();
+				foreach($ref->getParameters() as $p){
+					$prms[] = '$' . $p->getName() . (
+						$p->isDefaultValueAvailable()
+							? (
+								' = <span class="'.$cls.'_mv">' . (
+									$p->isDefaultValueConstant()
+										? $p->getDefaultValueConstantName()
+										: var_export($p->getDefaultValue(), true)
+								) . '</span>'
+							)
+							: ''
+					);
+				}
+			}else{
+				$doc = null;
+				$prms = array('???');
+			}
+			
+			$xcls = !$doc ? $cls.'_ad' : '';
+			$html  = '<a class="'.$cls.'_c" '.($doc?'href="javascript:;"':'').' onclick="'.$this->js_func.'(\''.$this->html_id.'\',\''.$id.'\');">';
+			$html .= '	<span class="'.$cls.'_a '.$xcls.'">&#9658;</span>';
+			$html .= '	<span class="'.$cls.'_k">'.$this->_esc_html($name).'<span class="'.$cls.'_ma">(<span>'.implode(', ', $prms).'</span>)</span></span>';
+			$html .= '</a>';
+			
+			if($doc){
+				$html .= '<div id="'.$this->html_id.'_v'.$id.'" class="nice_r_v '.$this->css_class.'_t_comment">';
+				$html .= nl2br(str_replace(' ', '&nbsp;', $this->_esc_html($doc)));
+				$html .= '</div>';
 			}
 			
 			return $html;
