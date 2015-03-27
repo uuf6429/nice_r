97b3ea325124fac7fe8df7061d1b8daccc44778b
Christian Sciberras <christian@sciberras.me>
2015-03-28 00:18:41 +0100

    Better method inspection

--- nice_r.js
+++ nice_r.js
@@ -1,12 +1,13 @@
-function nice_r_toggle(pfx, id){
-	var el = document.getElementById(pfx+'_v'+id);
-	if(el){
-		if(el.style.display==='block'){
-			el.style.display = 'none';
-			document.getElementById(pfx+'_a'+id).innerHTML = '&#9658;';
-		}else{
-			el.style.display = 'block';
-			document.getElementById(pfx+'_a'+id).innerHTML = '&#9660;';
-		}
-	}
+function nice_r_toggle(pfx, id) {
+    var elp = document.getElementById(pfx + '_v' + id);
+    var elc = document.getElementById(pfx + '_a' + id);
+    if (elp) {
+        if (elp.style.display === 'block') {
+            elp.style.display = 'none';
+            if (elc) elc.innerHTML = '&#9658;';
+        } else {
+            elp.style.display = 'block';
+            if (elc) elc.innerHTML = '&#9660;';
+        }
+    }
 }
--- nice_r.php
+++ nice_r.php
@@ -1,15 +1,15 @@
 <?php
 
-	/**
-	 * Nicely prints human-readable information about a value.
-	 * Note: This funciton is provided for compatibility reasons with earlier version.
-	 * You don't need this file if you plan on using the class directly.
-	 * @param mixed $value The value to print.
-	 * @param bool $return (Optional) Return printed HTML instead of writing it out (default is false).
-	 * @return string If $return is true, the rendered HTML otherwise null.
-	 */
-	function nice_r($value, $return = false){
-		$n = new Nicer($value);
-		return $return ? $n->generate() : $n->render();
-	}
+/**
+ * Nicely prints human-readable information about a value.
+ * Note: This funciton is provided for compatibility reasons with earlier version.
+ * You don't need this file if you plan on using the class directly.
+ * @param mixed $value The value to print.
+ * @param bool $return (Optional) Return printed HTML instead of writing it out (default is false).
+ * @return string If $return is true, the rendered HTML otherwise null.
+ */
+function nice_r($value, $return = false){
+	$n = new Nicer($value);
+	return $return ? $n->generate() : $n->render();
+}
 	
